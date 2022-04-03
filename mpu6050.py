f73a0c653280de61c880fb611f7db48384b599b7
ismailAkbulut1 <102900503+ismailAkbulut1@users.noreply.github.com>
2022-04-03 12:42:10 +0300

    Update mpu6050.py

--- mpu6050.py
+++ mpu6050.py
@@ -1,12 +1,13 @@
 from imu import MPU6050
 import time
 from machine import Pin, I2C
-
+#raspberry pi pico üzerindeki bağlantı pinleri aşağıdadır
 i2c = I2C(1, sda=Pin(18), scl=Pin(19), freq=400000)
 imu = MPU6050(i2c)
 
 while True:
     #ani ivmeyi yazdırır
+    #Kullanmak için aşağıdaki yorum satırını kaldırın
     #print('Gx=',imu.gyro.x,"",'Gy=',imu.gyro.y,"",'Gz=',imu.gyro.z,"")
     
     #koordinat değişimini yazdırır
